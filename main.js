517aceadee113231ffb219383f8787d30059e14b
Lyudmila1710 <135807864+Lyudmila1710@users.noreply.github.com>
2023-12-06 14:13:13 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+    window.location.href = "login.html";
+  });
+
+  document.getElementById("RegistrationButton").addEventListener("click", function() {
+    window.location.href = "registration.html";
+  });
+
+  document.getElementById("rat").addEventListener("click", function() {
+    window.location.href = "post.html";
+  });
